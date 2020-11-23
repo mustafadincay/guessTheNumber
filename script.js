343d804a2274eff50ce7b188cdf7f6473a5fc8a2
Your Name <you@example.com>
2020-11-24 00:18:32 +0300

    again button is working now

--- script.js
+++ script.js
@@ -8,8 +8,8 @@
 
 /*let temp = document.querySelector('.guess').value;*/
 
-const secretNumber = Math.trunc(Math.random() * 20 + 1);
-document.querySelector('.number').textContent = secretNumber;
+let secretNumber = Math.trunc(Math.random() * 20 + 1);
+//document.querySelector('.number').textContent = secretNumber; //this makes visible the secret number!
 let score = 20;
 let highscore = 20;
 document.querySelector('.check').addEventListener('click', function () {
@@ -24,6 +24,7 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
     document.querySelector('.number').style.fontSize = '10rem';
+    document.querySelector('.number').textContent = secretNumber;
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = `Too high!📈`;
@@ -49,4 +50,17 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = 0;
     }
   }
+
+  document.querySelector('.again').addEventListener('click', function () {
+    score = 20;
+    highscore = 20; //this will be change
+    secretNumber = Math.trunc(Math.random() * 20 + 1);
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.message').textContent = 'Start guessing...';
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.guess').value = '';
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.number').style.width = '15rem';
+    document.querySelector('.number').style.fontSize = '5rem';
+  });
 });
